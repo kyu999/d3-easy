acd9c1daa410b51bc82309445cfd4161b1d09d9f
kyu999 <kyukokkyou999@gmail.com>
2015-06-09 13:43:30 +0900

    Update demo.js

--- demo.js
+++ demo.js
@@ -2,7 +2,10 @@
 
 $().ready(function(){
 
+    // Make sure the format => [ {"word": String, "value": Number}, ..., ... ]
     data = [{"word": "XXX", "value": 3}, {"word": "YYY", "value": 13}, {"word": "ZZZ", "value": 8}]
+    
+    //makeWordCloud(data, css selector that you wanna insert in, scale of svg)
     window.makeWordCloud(data, "body", 500)
 
 })
